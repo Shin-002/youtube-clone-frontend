de0a5e6b7cf33b5667c7879311181764efd7fc67
Shin-002 <anko13ko@yahoo.co.jp>
2020-07-02 05:25:09 +0900

    create loginform

--- src/components/Login.js
+++ src/components/Login.js
@@ -44,13 +44,157 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
-const Login = () => {
+  const initialState = {
+      isLoading: false,
+      isLoginView: true,
+      error: '',
+      credentialsLog: {
+          email: '',
+          password: ''
+      },
+  };
+
+  const loginReducer = (state, action) => {
+      switch (action.type) {
+          case START_FETCH: {
+              return {
+                  ...state,
+                  isLoading: true,
+              };
+          }
+          case FETCH_SUCCESS: {
+              return {
+                  ...state,
+                  isLoading: false,
+              };
+          }
+          case ERROR_CATCHED: {
+              return {
+                  ...state,
+                  error: 'Email or password is not correct !',
+                  isLoading: false,
+              };
+          }
+          case INPUT_EDIT: {
+              return {
+                ...state,
+                [action.inputName]: action.payload,
+                error: '',
+              };
+          }
+          case TOGGLE_MODE: {
+              return {
+                  ...state,
+                  isLoginView: !state.isLoginView,
+              };
+          }
+          default:
+              return state;
+      }
+  }
+
+const Login = (props) => {
     const classes = useStyles();
+    const [state, dispatch] = useReducer(loginReducer, initialState);
+
+    const inputChangedLog = () => event => {
+        const cred = state.credentialsLog;
+        cred[event.target.name] = event.target.value;
+        dispatch({
+            type: INPUT_EDIT,
+            inputName: 'state.credentialLog',
+            payload: cred,
+        })
+    }
+
+    const login = async(event) => {
+        event.preventDefault()
+        if(state.isLoginView) {
+            try {
+                dispatch({type: START_FETCH})
+                const res = await axios.post(`http://127.0.0.1:8000/authen/jwt/create/`,state.credentialsLog, {
+                headers: {'Content-Type': 'application/json'}})
+                props.cookies.set('jwt-token', res.data.access);
+                res.data.access ? window.location.href = "/youtube" : window.location.href = "/";
+                dispatch({type: FETCH_SUCCESS})
+            } catch {
+                dispatch({type: ERROR_CATCHED});
+            }
+        } else {
+            try {
+                dispatch({type: START_FETCH})
+                await axios.post(`http://127.0.0.1:8000/api/create/`,state.credentialsLog, {
+                headers: {'Content-Type': 'application/json'},})
+
+                const res = await axios.post(`http://127.0.0.1:8000/authen/jwt/create/`,state.credentialsLog, {
+                headers: {'Content-Type': 'application/json'}})
+                props.cookies.set('jwt-token', res.data.access);
+                res.data.access ? window.location.href = "/youtube" : window.location.href = "/";
+                dispatch({type: FETCH_SUCCESS})
+            } catch {
+                dispatch({type: ERROR_CATCHED});
+            }
+        }
+    }
+
+    const toggleView = () => {
+        dispatch({type: TOGGLE_MODE})
+    }
+
     return (
-        <div>
+        <Container maxWidth="xs">
+            <form onSubmit={login}>
+                <div className={classes.paper}>
+                {state.isLoading && <CircularProgress />}
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5">
+                    {state.isLoginView ? 'Login' : 'Register'}
+                </Typography>
+
+                <TextField
+                    variant="outline" margin="normal"
+                    fullWidth label="Email"
+                    name="email"
+                    value={state.credentialsLog.email}
+                    onChange={inputChangedLog()}
+                    autoFocus/>
+
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    name="password"
+                    value={state.credentialsLog.password}
+                    onChange={inputChangedLog()}
+                    label="Password"
+                    type="password"/>
+
+                <span className={classes.spanError}>{state.error}</span>
+
+                { state.isLoginView ? 
+                    !state.credentialsLog.password || !state.credentialsLog.email ?
+                    <Button className={classes.submit} type="submit" fullWidth disabled
+                        variant="contained" color="primary">Login</Button>
+                    : <Button className={classes.submit} type="submit" fullWidth
+                        variant="contained" color="primary">Login</Button>
+                  :
+                    !state.credentialsLog.password || !state.credentialsLog.email ?
+                    <Button className={classes.submit} type="submit" fullWidth disabled
+                        variant="contained" color="primary">Register</Button>
+                    : <Button className={classes.submit} type="submit" fullWidth
+                        variant="contained" color="primary">Register</Button>
+                }
+
+                <span onClick={()=>toggleView()} className={classes.span}>
+                    {state.isLoginView ? 'Create Account' : 'Back to login'}
+                </span>
             
-        </div>
+                </div>
+            </form>
+        </Container>
     )
 }
 
-export default Login
+export default withCookies(Login)
